281267c8c212e355a5d7e6977943e9152036bcda
SashaKor <alexandra.korukova@gmail.com>
2018-10-28 15:10:15 +0100

    small test example

--- models/users.js
+++ models/users.js
@@ -1,10 +1,10 @@
-const mongoose = require('mongoose');
+const mongoose = require('mongoose')
 
-const Schema = mongoose.Schema;
+const Schema = mongoose.Schema
 
 const users = new Schema({
   _id: Schema.Types.ObjectId,
   gitId: String
-}, { collection: 'user' });
+}, {collection: 'user'})
 
 module.exports = mongoose.model('user', users);
